76acfb28f7d732ecd62f3e171a84ed9f3da8c23d
hasibdurjoy <hrd2571@gmail.com>
2021-10-19 13:29:29 +0600

    problem solved

--- src/pages/home/Banner/Banner/Banner.js
+++ src/pages/home/Banner/Banner/Banner.js
@@ -1,25 +1,21 @@
 import React, { useEffect, useState } from 'react';
-import { Carousel } from 'react-bootstrap';
 import './Banner.css';
-import BannerCarousel from '../BannerCarousel/BannerCarousel';
 
 const Banner = () => {
-    const [banners, setBanners] = useState([])
-    useEffect(() => {
-        fetch('banners.json')
-            .then(res => res.json())
-            .then(data => setBanners(data))
-    }, []);
     return (
-        <div className="banner-container">
-            <Carousel>
-                {
-                    banners.map(banner => <Carousel.Item>
-                        <BannerCarousel key={banner.id} banner={banner}></BannerCarousel>
-                    </Carousel.Item>)
-                }
-
-            </Carousel>
+        <div className="banner-container pt-3">
+            <div className="row">
+                <div className="col-lg-6 col-md-6 col-12">
+                    <img src="https://i.ibb.co/jzyrtQq/Screenshot-3-removebg-preview.png" alt="" />
+                </div>
+                <div className="col-lg-6 col-md-6 col-12 d-flex align-items-center pe-5">
+                    <div>
+                        <h2>Doctor</h2>
+                        <p>Description</p>
+                        <button className="btn btn-danger rounded-pill">Book Now</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
